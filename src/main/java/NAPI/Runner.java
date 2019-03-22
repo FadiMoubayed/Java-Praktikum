dfcb6254e1b25bb8abfa4d858d0e420cf0868fee
paulascharf@gmx.de <paulascharf@gmx.de>
2019-03-22 10:54:26 +0100

    made the jar execution work and some other minor changes (e.g. new exception in GuiController)

--- src/main/java/NAPI/Runner.java
+++ src/main/java/NAPI/Runner.java
@@ -8,7 +8,7 @@ import NAPI.View.GUIView;
  * This is the runner class for our NavigationApi.
  * You can choose between a Graphical User Interface and a Console Line Interface.
  * @author Stefan, Fadi, Thomas, Paula
- * @version 1.0
+ * @version 0.99
  */
 public class Runner {
     public static void main(String[] args)
